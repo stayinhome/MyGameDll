2d45e4d59750bd89caf609f9a7832b42606dbe8b
远晖 罗 <871844240@qq.com>
2021-06-13 16:40:26 +0800

    新增

---
+++ MyGameDll/GameEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameDll
+{
+
+
+    public class GameEnum
+    {
+        public enum Layer
+        {
+            Chess = 7,
+
+            Node = 6,
+
+        }
+
+
+
+    }
+}
