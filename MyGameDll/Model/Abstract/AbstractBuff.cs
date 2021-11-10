c268642fce99ddfbde5fcbc49db0c53b0998a0b7
远晖 罗 <871844240@qq.com>
2021-11-10 18:55:24 +0800

    114

---
+++ MyGameDll/Model/Abstract/AbstractBuff.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MyGameDll.Model.Abstract
+{
+    public abstract class AbstractBuff 
+    {
+        /// <summary>
+        /// 生效次数
+        /// </summary>
+        public int Count = 0;
+
+        /// <summary>
+        /// 使用目标
+        /// </summary>
+        public GameObject Target = null;
+
+        /// <summary>
+        /// 是否需要目标
+        /// </summary>
+        public bool NeedTarget = false;
+
+
+        /// <summary>
+        /// 具体效果
+        /// </summary>
+        /// <param name="Target"></param>
+        public abstract void Buff(GameObject Target);
+
+
+
+
+
+    }
+}
