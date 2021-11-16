0416a7da75f48e7c59b293309635df78f477bb7a
远晖 罗 <871844240@qq.com>
2021-11-16 18:01:59 +0800

    41241

---
+++ Common/Environment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Environment
+    {
+
+        /// <summary>
+        /// 日志打印开关
+        /// </summary>
+        public static bool M_Log = false;
+
+        public static string PlayerID = "";
+
+
+
+
+    }
+}
---
+++ DataBaseHelper/DataBaseHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseHelper
+{
+    public class DataBaseHelper
+    {
+        private string _ConnectionString = "";
+        private SqlConnection _sqlConnection = new SqlConnection();
+
+        private void OpenDatabase()
+        {
+            try
+            {
+
+                if(_sqlConnection.State == System.Data.ConnectionState.Closed)
+                {
+                    _sqlConnection.ConnectionString = _ConnectionString;
+                    _sqlConnection.Open();
+                }
+
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+        }
+
+        private void CloseDatabase()
+        {
+            try
+            {
+                if(_sqlConnection.State != System.Data.ConnectionState.Closed)
+                {
+                    this._sqlConnection.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public DataTable GetDataTable(string sql)
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                OpenDatabase();
+                SqlCommand cmd = new SqlCommand(sql, this._sqlConnection);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataTable = ds.Tables[0];
+
+                CloseDatabase();
+            }catch(Exception e)
+            {
+
+            }
+
+            return dataTable;
+
+
+        }
+
+
+    }
+}
